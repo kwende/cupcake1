f759fc5ae74272d5a95005e964efeb446192ec7d
Ben Rush <brush@nebraskaglobal.com>
2015-07-06 13:10:55 -0500

    scripts and whatnot.

---
+++ Assets/Scripts/DestroyObjectAndAddPoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObjectAndAddPoint : MonoBehaviour {
+
+	void Start()
+	{
+		MyScore = 0; 
+	}
+
+	public int MyScore {get;private set;}
+
+	void OnTriggerEnter2D(Collider2D collisionObject)
+	{
+		Destroy (collisionObject.gameObject); 
+
+		MyScore++; 
+
+		Debug.Log ("My score is " + MyScore.ToString ()); 
+	}
+}
---
+++ Assets/Scripts/SpawnOneObject.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnOneObject : MonoBehaviour {
+
+	private Vector3 _startPosition; 
+
+	public float MoveSpeed; 
+
+	public float MoveDistance; 
+
+	public GameObject RandomGameObject; 
+
+	private int _counter = 0; 
+
+	private bool _goingRight = true; 
+
+	// Use this for initialization
+	void Start () {
+		MoveSpeed = 1.0f; 
+		MoveDistance = 4.0f; 
+
+		_startPosition = transform.position; 
+
+		GameObject myGameObject = Instantiate (RandomGameObject); 
+		myGameObject.transform.position = transform.position; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_counter++; 
+
+		if (Random.value < .1)
+		{
+			GameObject myGameObject = Instantiate (RandomGameObject); 
+			myGameObject.transform.position = transform.position; 
+		}
+
+		if (_counter % 10 == 0) {
+			_goingRight = !_goingRight; 
+		}
+
+		if (_goingRight) {
+			transform.position = new Vector3(transform.position.x + 1, 
+			                                 transform.position.y,
+			                                 transform.position.z); 
+		} else {
+			transform.position = new Vector3(transform.position.x - 1, 
+			                                 transform.position.y,
+			                                 transform.position.z); 
+		}
+	}
+}
